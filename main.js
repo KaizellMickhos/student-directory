a3e8cab631ba0d54bfaaffdd828351baf8fd6ea6
Kaylla Mae Bismanos <alykabismanos@gmail.com>
2026-05-13 20:58:27 +0800

    Add Kaylla Mae Bismanos profile to homepage

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ const students = [
     linkedin: "https://linkedin.com/in/johndoe",
     facebook: "https://facebook.com/johndoe"
   }, "Business", "JD"),
-  createStudent("Mark Jun Gersaniva", "https://via.placeholder.com/200", "Aspiring software engineer with a passion for open-source projects and community building.", {
-    github: "https://github.com/nujkram",
-    linkedin: "https://linkedin.com/in/nujkram",
-  }, "Engineering", "MJ")
+  createStudent("Kaylla Mae Bismanos", "https://via.placeholder.com/200", " Computer Science student passionate about web development and Programming.", {
+    github: "https://github.com/KayllaMaeBismanos",
+    linkedin: "https://linkedin.com/in/KayllaMaeBismanos",
+  }, "Computer Science student", "KMB")
 ];
 
 // Generate social icons
