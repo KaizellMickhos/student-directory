9e9547657e21bd81c1e354af9368fcb629e19b6c
Elmowen17 <elmowenmontano@gmail.com>
2026-05-16 19:13:25 +0800

    Add Elmowen Montaño profilegit status

--- main.js
+++ main.js
@@ -23,7 +23,11 @@ const students = [
   createStudent("Mark Jun Gersaniva", "https://via.placeholder.com/200", "Aspiring software engineer with a passion for open-source projects and community building.", {
     github: "https://github.com/nujkram",
     linkedin: "https://linkedin.com/in/nujkram",
-  }, "Engineering", "MJ")
+  }, "Engineering", "MJ"),
+  createStudent("Elmowen Montaño", "https://via.placeholder.com/200", "Aspiring software engineer with a passion for open-source projects and community building.", {
+  github: "https://github.com/Elmowen17",
+  facebook: "https://facebook.com/phil.pacs17",
+  }, "Programmer", "EM")
 ];
 
 // Generate social icons
