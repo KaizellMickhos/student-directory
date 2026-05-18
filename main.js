3dda1a28b1cc884b9bd3e5d7faf4f2aad294b104
andrei735 <andreidumol79@gmail.com>
2026-05-18 12:34:27 +0800

    Add Andrei Dumol profile to homepage

--- main.js
+++ main.js
@@ -23,7 +23,11 @@ const students = [
   createStudent("Mark Jun Gersaniva", "https://via.placeholder.com/200", "Aspiring software engineer with a passion for open-source projects and community building.", {
     github: "https://github.com/nujkram",
     linkedin: "https://linkedin.com/in/nujkram",
-  }, "Engineering", "MJ")
+  }, "Engineering", "MJ"),
+    createStudent("Andrei Dumol", "https://via.placeholder.com/200", "Aspiring software engineer with a passion for open-source projects and community building.", {
+    github: "https://github.com/andrei735",
+    facebook: "https://web.facebook.com/andrei.dumol.79"
+  }, "Programmer", "AD")
 ];
 
 // Generate social icons
