e0fe3b6b9912e4eb3ea0f732fcfc4529c88e4a5f
dejerofranzen-lgtm <dejerofranzen@gmail.com>
2026-05-18 15:21:45 +0800

    Add Franzen Mae Dejero profile to homepage

--- main.js
+++ main.js
@@ -53,6 +53,10 @@ const students = [
     linkedin: "https://linkedin.com/in/mikealdrendizon",
     facebook: "https://facebook.com/mikealdrendizon"
   }, "Design", "MD"),
+    createStudent("Franzen Mae Dejero", "https://via.placeholder.com/200", "Creative designer specializing in branding and digital media with a keen eye for aesthetics.", {
+    github: "https://github.com/dejerofranzen-lgtm",
+    facebook: "https://www.facebook.com/franzen.mae.dejer0"
+  }, "Design", "FD")
 ];
 
 // Generate social icons
