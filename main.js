f70326d150560d308c5c1a24701ef796329bd833
rpgxfuentes-dotcom <rpgxfuentes@gmail.com>
2026-05-30 13:57:49 +0800

    Add Frank Fuentes profile to homepage

--- main.js
+++ main.js
@@ -53,6 +53,10 @@ const students = [
     linkedin: "https://linkedin.com/in/mikealdrendizon",
     facebook: "https://facebook.com/mikealdrendizon"
   }, "Design", "MD"),
+  createStudent("Frank Fuentes", "https://via.placeholder.com/200", "Creative designer specializing in branding and digital media with a keen eye for aesthetics.", {
+    github: "https://github.com/rpgxfuentes-dotcom",
+    facebook: "https://www.facebook.com/frank.fuentes.549668"
+  }, "Design", "FF")
 ];
 
 // Generate social icons
